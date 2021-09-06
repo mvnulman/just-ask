2464acdf36c800bb9d1ec669e359792945084524
Marcos Vinicius <mvsouza7@gmail.com>
2021-09-06 16:06:15 +0300

    Page NewRoom added and stylized

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+// import googleIconImg from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+
+export function NewRoom () {
+    return  (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Q&A illustration" />
+                <strong>Create Q&amp;A live rooms</strong>
+                <p>Ask your audience questions in real time</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="just ask logo" />
+                    <h2>Create a new room</h2>
+                    <form>
+                        <input type="text"
+                        placeholder="Room's name"
+                        />
+                        <Button type="submit">
+                        Create Room
+                        </Button>
+                    </form>
+                    <p>
+                    Want to join an existing room? <a href="#">Click here</a>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
