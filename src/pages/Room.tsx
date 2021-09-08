9c608247c464b544c748678c976b9822b7129487
Marcos Vinicius <mvsouza7@gmail.com>
2021-09-08 14:56:13 +0300

    RoomCode and Room component created with HTML + CSS done

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,7 +1,42 @@
+import { useParams } from "react-router-dom";
+
+import logoImg from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import '../styles/room.scss'
+
+type RoomParams = {
+  id: string;
+}
+
+
 export function Room() {
-    return (
-        <div>
-            <h1>Room</h1>
+  const params = useParams<RoomParams>();
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="just ask logo" />
+          <RoomCode code={params.id}/>
         </div>
-    )
+      </header>
+
+      <main className="content">
+          <div className="room-title">
+              <h1>React Room</h1>
+              <span>4 questions</span>
+          </div>
+          <form>
+              <textarea
+                placeholder="What you would like to ask?"
+              />
+            <div className="form-footer">
+            <span>To submit questions, <button>login</button>.</span>
+            <Button type="submit">Submit question</Button>
+            </div> 
+          </form>
+      </main>
+    </div>
+  );
 }
